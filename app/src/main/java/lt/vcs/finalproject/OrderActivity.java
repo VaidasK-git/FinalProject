1219d5b3fbcd456f2fe3de89267f9d836db185bd
Dailius <dailius.rascius@gmail.com>
2022-04-20 13:18:20 +0300

    Fetch Formula data from DAO

--- app/src/main/java/lt/vcs/finalproject/OrderActivity.java
+++ app/src/main/java/lt/vcs/finalproject/OrderActivity.java
@@ -1,40 +1,42 @@
 package lt.vcs.finalproject;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import lt.vcs.finalproject.repository.Customer;
-import lt.vcs.finalproject.repository.CustomerDao;
-import lt.vcs.finalproject.repository.FormulaDao;
-import lt.vcs.finalproject.repository.MainDatabase;
-import lt.vcs.finalproject.repository.OrderDao;
-import lt.vcs.finalproject.repository.OrderDetails;
-import lt.vcs.finalproject.repository.OrderDetailsDao;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import lt.vcs.finalproject.repository.local.FormulaDao;
+import lt.vcs.finalproject.repository.local.MainDatabase;
+import lt.vcs.finalproject.repository.local.OrderDao;
+import lt.vcs.finalproject.repository.model.Formula;
+import lt.vcs.finalproject.repository.model.Order;
 
 public class OrderActivity extends AppCompatActivity {
 
     private Button closeButton;
     private Intent intent;
-    private OrderDetails orderDetails;
+    //    private OrderDetails orderDetails;
+    private Order orderDetails;
+    private Formula formulaDetails;
 
-    private OrderDetailsDao orderDetailsDao;
+    //    private OrderDetailsDao orderDetailsDao;
+    private OrderDao orderDao;
+    private FormulaDao formulaDao;
 
     private TextView firstAndLastNameTextView;
     private TextView oxidantsAndColorsTextView;
     private TextView timeAndPriceTextView;
     private AlertDialog.Builder builder;
 
-    int customerId;
+    private int customerId;
+    private int formulaId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +53,25 @@ public class OrderActivity extends AppCompatActivity {
         customerId = intent.getIntExtra("lt.vcs.finalproject.mainactivity.customerid", 0);
 
         MainDatabase mainDatabase = MainDatabase.getInstance(getApplicationContext());
-        orderDetailsDao = mainDatabase.orderDetailsDao();
+//        orderDetailsDao = mainDatabase.orderDetailsDao();
+        orderDao = mainDatabase.orderDao();
 
-        orderDetails = orderDetailsDao.getItem(customerId);
-        firstAndLastNameTextView.setText("Customer: " + orderDetails.getCustomerFirstName() + " " + orderDetails.getCustomerLastName() + " " + orderDetails.getCustomerPhoneNumber());
-        oxidantsAndColorsTextView.setText("Oxidants: " + orderDetails.getOxidants() + "\nColors: " + orderDetails.getColors());
-        timeAndPriceTextView.setText("Time: " + orderDetails.getFormulaTime() + "\nPrice: " + orderDetails.getFormulaPrice());
+        orderDetails = orderDao.getItem(customerId);
+        formulaId = orderDetails.getFormulaId();
+
+        formulaDao = mainDatabase.formulaDao();
+        formulaDetails = formulaDao.getItem(formulaId);
+
+//        getPlainTextFromList(formulaDetails.getOxidants());
+
+//        firstAndLastNameTextView.setText("Customer: " + orderDetails.getCustomerFirstName() + " " + orderDetails.getCustomerLastName() + " " + orderDetails.getCustomerPhoneNumber());
+
+        oxidantsAndColorsTextView.setText(
+                "Oxidants: \n" +
+                        getPlainTextFromList(formulaDetails.getOxidants()) +
+                        "\n\nColors:\n" +
+                        getPlainTextFromList(formulaDetails.getColors()));
+//        timeAndPriceTextView.setText("Time: " + orderDetails.getFormulaTime() + "\nPrice: " + orderDetails.getFormulaPrice());
 
     }
 
@@ -68,14 +83,11 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     private void oxidantsAndColorsTextViewClick() {
-        oxidantsAndColorsTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setUpMessageBuilder();
-                android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                android.content.ClipData clip = android.content.ClipData.newPlainText("text label", oxidantsAndColorsTextView.getText());
-                clipboard.setPrimaryClip(clip);
-            }
+        oxidantsAndColorsTextView.setOnClickListener(view -> {
+            setUpMessageBuilder();
+            android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            android.content.ClipData clip = android.content.ClipData.newPlainText("text label", oxidantsAndColorsTextView.getText());
+            clipboard.setPrimaryClip(clip);
         });
     }
 
@@ -88,11 +100,14 @@ public class OrderActivity extends AppCompatActivity {
 
     private void setUpCloseButton() {
         closeButton = findViewById(R.id.closeButtonOrder);
-        closeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        closeButton.setOnClickListener(view -> finish());
+    }
+
+    private <T> String getPlainTextFromList(List<T> list) {
+        String resultText = "";
+        for (T item: list){
+            resultText += item.toString() + "\n";
+        }
+        return resultText;
     }
 }
